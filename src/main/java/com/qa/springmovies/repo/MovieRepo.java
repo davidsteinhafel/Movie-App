954c264cda5a3c5f210f892241099d405c87d748
davidsteinhafel <dasteinhafel@gmail.com>
2021-06-11 13:00:38 -0500

    set up interface repo extending JpaRepo

--- src/main/java/com/qa/springmovies/repo/MovieRepo.java
+++ src/main/java/com/qa/springmovies/repo/MovieRepo.java
@@ -1,24 +1,11 @@
 package com.qa.springmovies.repo;
 
-public class MovieRepo {
-	package com.qa.movies.repo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-	import java.util.List;
+import com.qa.springmovies.domain.Movie;
 
-	import org.springframework.data.jpa.repository.JpaRepository;
-	import org.springframework.data.jpa.repository.Query;
-	import org.springframework.stereotype.Repository;
-
-	import com.qa.movies.domain.Movie;
-
-	@Repository
-	public interface MovieRepo extends JpaRepository<Movie, Long> {
-		
-		// SELECT * from Movie where name = '[name]';
-		List<Movie> findAnimalByName(String name);
-		
-		@Query(value = "SELECT * from Movie WHERE name = ?Puma and species = ?Cat", nativeQuery = true)
-		 Movie findAnimalByNameAndSpeciesSQL(String name, String species); //name is parameter 1 and age is 2.
-	}
-
+@Repository
+public interface MovieRepo extends JpaRepository<Movie, Long> {
+	
 }
